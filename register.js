944c9dbff84bca4cc4a11ab60ff9fe206330548e
abdulreadhido-maker <abdulreadhido@gmail.com>
2026-07-02 17:05:26 +0300

    Update register.js

--- register.js
+++ register.js
@@ -1,1 +1,43 @@
+const form = document.getElementById("registerForm");
 
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const full_name = document.getElementById("full_name").value;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password !== confirmPassword) {
+        alert("كلمتا المرور غير متطابقتين");
+        return;
+    }
+
+    try {
+        const response = await fetch("http://localhost:3000/api/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                full_name,
+                username,
+                email,
+                password
+            })
+        });
+
+        const data = await response.json();
+
+        alert(data.message);
+
+        if (data.success) {
+            window.location.href = "login.html";
+        }
+
+    } catch (error) {
+        alert("تعذر الاتصال بالخادم.");
+        console.error(error);
+    }
+});
